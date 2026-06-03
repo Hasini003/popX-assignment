15526a10d988349be74d5a3871b4790e243340fe
Hasini003 <hasini43077@gmail.com>
2026-06-03 19:11:51 +0530

    Home Test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,7 @@
-import { useNavigate } from "react-router-dom";
-
 function Home() {
-  const navigate = useNavigate();
-
   return (
-    <div className="mobile-container">
-      <div className="home-content">
-        <h1>Welcome to PopX</h1>
-
-        <p>
-          Lorem ipsum dolor sit amet,
-          <br />
-          consectetur adipiscing elit.
-        </p>
-
-        <button
-          className="primary-btn"
-          onClick={() => navigate("/register")}
-        >
-          Create Account
-        </button>
-
-        <button
-          className="secondary-btn"
-          onClick={() => navigate("/login")}
-        >
-          Already Registered? Login
-        </button>
-      </div>
+    <div>
+      <h1>HOME TEST</h1>
     </div>
   );
 }
